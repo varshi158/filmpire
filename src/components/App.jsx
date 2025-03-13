e0d38b22aa119b03ead7423b2155c32e064a73ec
Varshika Dhevanayagam <varshika.20210977@iit.ac.lk>
2025-03-13 15:28:10 +0530

    styling setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
 import { CssBaseline } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 
+import useStyles from "./styles";
 import { Movies, MovieInformation, Actors, Profile, Navbar } from "./component-exports";
 
 
-
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
+    <div className={classes.root}>
       <CssBaseline />
       <Navbar />
-      <main>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Routes>
           <Route path="/movie/:id" element={<MovieInformation />} />
           <Route path="/actors/:id" element={<Actors />} />
