44f939648ac0f6d4098c7f98576962b6f23d78e8
Varshika Dhevanayagam <varshika.20210977@iit.ac.lk>
2025-03-14 23:57:16 +0530

    finish movie view

--- src/components/Movie/styles.jsx
+++ src/components/Movie/styles.jsx
@@ -4,8 +4,29 @@ export default makeStyles((theme) => ({
     movie: {
         padding: '10px',
     },
+    links: {
+        alignItems: 'center',
+        fontWeight: 'bolder',
+        textDecoration: 'none',
+        [theme.breakpoints.up('xs')]: {
+            display: 'flex',
+            flexDirection: 'column',
+        },
+        '&:hover': {
+            cursor: 'pointer',
+        },
+    },
+    image: {
+        borderRadius: '10px',
+        height: '300px',
+        marginBottom: '10px',
+        '&:hover': {
+            transform: 'scale(1.05)', // Ensures scaling on hover
+            cursor: 'pointer',
+        },
+    },
     title: {
-        color: theme.palette.primary.primary,
+        color: theme.palette.text.primary,
         textOverflow: 'ellipsis',
         width: '230px',
         whiteSpace: 'nowrap',
